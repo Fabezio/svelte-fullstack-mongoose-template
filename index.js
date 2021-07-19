f75cf991f3c29cb49127a4ff880d01dc20f35b75
fabezio <fabezio@outlook.fr>
2021-07-20 00:32:48 +0200

    passage en mode distant (database)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const app = require('./api/srv')
 
-mongoose.connect('mongodb://localhost/workplaces', {
+mongoose.connect('mongodb+srv://fabezio:C0denCQRT@cluster0.0r1tc.mongodb.net/timemap?retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
